e58dd6f766d5f7848e2037be2680cdeb3c3eb254
hands8142 <hands8142@naver.com>
2020-10-27 15:38:07 +0900

    make it better

--- app.py
+++ app.py
@@ -148,4 +148,4 @@ api.add_resource(corona, "/corona")
 api.add_resource(home, "/")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80, debug=True)
+    app.run(host="0.0.0.0", port=80)
