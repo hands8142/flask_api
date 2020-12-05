3cb64b3a878140e07560cd0bea7a3d03efa6eef4
hands8142 <46277532+hands8142@users.noreply.github.com>
2020-12-05 10:26:45 +0900

    Update app.py

--- app.py
+++ app.py
@@ -83,7 +83,7 @@ class weather(Resource):
                 "최저최고온도" : f"{TodayMorningTemp}/{TodayAfternoonTemp}",
                 "미세먼지": FineDust,
                 "초미세먼지": UltraFineDust,
-                "오존 지수": Ozon
+                "오존지수": Ozon
                 }
 
 class music(Resource):
